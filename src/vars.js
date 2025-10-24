4cb1c6aad08bf275d934700fb2e0fc965fd017e5
Riccardo De Lucia <riccardoroberto.delucia@gmail.com>
2025-10-24 11:30:29 +0200

    SEO and refactor

---
+++ src/vars.js
@@ -0,0 +1,4 @@
+export const address = "Località Croix Noire 61, Saint-Christophe, AO 11020";
+export const email = "info@studiocasolavisentin.it";
+export const piva = "01088020076";
+export const phone = "+39 0165 267198 ";
